a3e10d5c742379d0815c29d23f2e838e21228630
FrK492 <faruk4928@gmail.com>
2022-09-14 21:52:15 +0300

    navigation added to project

---
+++ src/screens/DetailScreen.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import { View, Text } from 'react-native'
+
+interface IProps {}
+
+const DetailScreen: FC<IProps> = (props) => {
+    return (
+        <View>
+            <Text>Detail Screen</Text>
+        </View>
+    )
+}
+
+export default DetailScreen
---
+++ src/screens/ListScreen.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import { View, Text } from 'react-native'
+
+interface IProps {}
+
+const ListScreen: FC<IProps> = (props) => {
+    return (
+        <View>
+            <Text>List Screen</Text>
+        </View>
+    )
+}
+
+export default ListScreen
